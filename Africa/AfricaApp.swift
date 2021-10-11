3a9916c0ab978b26f8942d5b69eef1440971c783
Thomas George <private@thomas-george.com>
2021-10-11 06:46:32 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Thomas George on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
